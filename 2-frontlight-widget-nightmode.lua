d40b532ff6956e0f95999459bc03dfaf82c52daa
peterboda236 <130438938+peterboda236@users.noreply.github.com>
2026-05-15 07:53:11 +0200

    Add files via upload

---
+++ 2-frontlight-widget-nightmode.lua
@@ -0,0 +1,57 @@
+local FrontLightWidget = require("ui/widget/frontlightwidget")
+local Button = require("ui/widget/button")
+local CenterContainer = require("ui/widget/container/centercontainer")
+local Event = require("ui/event")
+local Geom = require("ui/geometry")
+local Size = require("ui/size")
+local TextWidget = require("ui/widget/textwidget")
+local VerticalGroup = require("ui/widget/verticalgroup")
+local VerticalSpan = require("ui/widget/verticalspan")
+local UIManager = require("ui/uimanager")
+local Device = require("device")
+local Screen = Device.screen
+local _ = require("gettext")
+
+local original_layout = FrontLightWidget.layout
+
+FrontLightWidget.layout = function(self)
+    original_layout(self)
+
+    self.nm_button = Button:new{
+        text = _("Toggle"),
+        width = self.button_width,
+        show_parent = self,
+        callback = function()
+            G_reader_settings:toggle("night_mode")
+            local new_state = G_reader_settings:isTrue("night_mode")
+            Screen:toggleNightMode()
+            UIManager:setDirty(nil, "full")
+            UIManager:broadcastEvent(Event:new("SetNightMode", new_state))
+            self:update()
+        end,
+    }
+
+    self.layout[#self.layout + 1] = { self.nm_button }
+
+    local function centered(widget)
+        return CenterContainer:new{
+            dimen = Geom:new{ w = self.inner_width, h = widget:getSize().h },
+            widget,
+        }
+    end
+
+    -- Felső elválasztó: Size.span.vertical_large * 4, ugyanannyi mint a warmth szekció előtt
+    -- Alsó padding: self.span * 2, hogy illeszkedjen a többi sor ritmusához
+    table.insert(self.frame[1], VerticalGroup:new{
+        VerticalSpan:new{ width = Size.span.vertical_large * 4 },
+        centered(TextWidget:new{
+            text = _("Night mode"),
+            face = self.medium_font_face,
+            bold = true,
+            max_width = self.inner_width,
+        }),
+        VerticalSpan:new{ width = self.span },
+        centered(self.nm_button),
+        VerticalSpan:new{ width = self.span * 2 },
+    })
+end
